d1a6edac92408191f6ed3c312da2a23480652489
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-11-26 12:42:26 -0400

    add user to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,13 +4,21 @@ const Usuario = require("../models/Usuario");
 const crearUsuario = async (req, res = response) => {
   //const { name, email, password } = req.body;
 
-  const usuario = new Usuario(req.body);
-  await usuario.save();
+  try {
+    const usuario = new Usuario(req.body);
+    await usuario.save();
 
-  res.status(201).json({
-    ok: true,
-    msg: "registro!!",
-  });
+    res.status(201).json({
+      ok: true,
+      msg: "registro!!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Porfavor hable con el administrador",
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
